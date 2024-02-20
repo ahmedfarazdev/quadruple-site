ddedd3868eecef9649c7ebe82ac7b8d835d4b407
Waqas <cpt.waqas@gmail.com>
2024-02-20 16:03:27 +0500

    Services Content Update

--- components/services/Services.js
+++ components/services/Services.js
@@ -13,13 +13,13 @@ export default function Services() {
     <section className={styles.services}>
       <div className="container">
         <h2 className="site-heading">About Us</h2>
-        <p className="site-text">All You Need To Know About Us</p>
+        <p className="site-text">Quadruple Solutions is a leading provider of innovative technology solutions, dedicated to empowering businesses across industries. With a commitment to excellence, we specialize in software development, IT consulting, project management, and bespoke technology solutions tailored to meet the unique needs of our clients. Our team of experts combines cutting-edge technology with industry insights to drive efficiency, streamline operations, and foster growth for our partners worldwide. Discover how Quadruple Solutions can transform your business today.</p>
         <div className={styles.services_card}>
           <div className={styles.single_card}>
             <div className={styles.icon_bg}>
               <BagIcon fill="white" width={30} height={28} />
             </div>
-            <h4>Web Development</h4>
+            <h4>Web Developers</h4>
             <p className={styles.card_text}>
             Elevate Your Online Presence with Expert Web Development Services
             </p>
@@ -31,7 +31,7 @@ export default function Services() {
             <div className={styles.icon_bg}>
               <OneIcon fill="white" width={32} height={30} />
             </div>
-            <h4>Mobile Apps Developement</h4>
+            <h4>Mobile Apps Developers</h4>
             <p className={styles.card_text}>
             Transform Your Ideas into Innovative Mobile Apps with Quadruple
             </p>
@@ -43,7 +43,7 @@ export default function Services() {
             <div className={styles.icon_bg}>
               <HouseIcon fill="white" width={32} height={30} />
             </div>
-            <h4>Blockchain Developement</h4>
+            <h4>UI/UX Designer</h4>
             <p className={styles.card_text}>
             Unlock the Power of Blockchain with Custom Development Solutions
             </p>
@@ -55,7 +55,7 @@ export default function Services() {
             <div className={styles.icon_bg}>
               <UserIcon fill="white" width={32} height={30} />
             </div>
-            <h4>Cloud Services</h4>
+            <h4>Cloud Service Engineers</h4>
             <p className={styles.card_text}>
             Harness the Power of AWS for Scalable and Secure Cloud Solutions
             </p>
