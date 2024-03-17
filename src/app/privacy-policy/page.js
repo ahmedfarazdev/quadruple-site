5216398ee77f1d62ccf37ebf7917f8e687803376
Waqas <cpt.waqas@gmail.com>
2024-03-17 19:00:54 +0500

    Content Updated

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -10,34 +10,38 @@ const page = () => {
       <section className={styles.privacy_policy}>
         <div className="container">
           <div className={styles.content_meta}>
-            <h4>Lorem Ipsum</h4>
+            
             <p>
-              The Securitynet.ai ecosystem is powered by the SecNet Token (SNT),
-              an ERC-20 compatible token built on the Ethereum blockchain. The
-              SecNet Token plays a pivotal role in facilitating access to our
-              AI-powered security services and governing the platform&apos;s
-              operations. Here&apos;s an overview of the tokenomics structure:
+            At Quadruple Solutions, we are committed to protecting the privacy and security of our clients' and visitors' personal information. This Privacy Policy outlines how we collect, use, disclose, and protect information gathered through our website and our services.
             </p>
-            <h4>Lorem Ipsum</h4>
+            <h4>Information We Collect</h4>
+            <p>We may collect personal information when you interact with us, including but not limited to:</p>
             <ul className={styles.list}>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
+              <li>Name, email address, phone number, and other contact details provided when you communicate with us or sign up for our services.</li>
+              <li>Information about how you interact with our website, including your IP address, browser type, pages visited, and other usage statistics collected through cookies and similar technologies.</li>
+              <li>Information provided by clients for the purpose of delivering our services, such as project requirements, specifications, and other relevant data.</li>
             </ul>
-            <h4>Lorem Ipsum</h4>
+            <h4>How We Use Your Information</h4>
             <p>
-              The Securitynet.ai ecosystem is powered by the SecNet Token (SNT),
-              an ERC-20 compatible token built on the Ethereum blockchain. The
-              SecNet Token plays a pivotal role in facilitating access to our
-              AI-powered security services and governing the platform&apos;s
-              operations. Here&apos;s an overview of the tokenomics structure:
+            We may use the information we collect for the following purposes:
             </p>
-            <h4>Lorem Ipsum</h4>
             <ul className={styles.list}>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
+              <li>To provide and improve our services, including personalized experiences and customer support.</li>
+              <li>To communicate with you about our services, promotions, and updates.</li>
+              <li>To analyze usage trends and optimize the performance of our website and services.</li>
+              <li>To comply with legal obligations and enforce our terms of service.</li>
             </ul>
+            <h4>Information Sharing and Disclosur</h4>
+            <p>We do not sell, rent, or trade your personal information to third parties. However, we may share your information in the following circumstances:</p>
+            <ul className={styles.list}>
+              <li>With service providers and business partners who assist us in delivering our services.</li>
+              <li>With your consent or at your direction, such as when you authorize the sharing of information with third-party integrations.</li>
+              <li>When required by law or to protect our rights, property, or safety, or the rights, property, or safety of others.</li>
+            </ul>
+            <h4>Data Security</h4>
+            <p>We implement appropriate technical and organizational measures to protect the confidentiality, integrity, and availability of your personal information. However, no method of transmission over the internet or electronic storage is completely secure, and we cannot guarantee absolute security.</p>
+            <h4>Changes to this Privacy Policy</h4>
+            <p>We reserve the right to update or modify this Privacy Policy at any time. We will notify you of any changes by posting the new Privacy Policy on this page. Your continued use of our website or services after any such changes constitutes acceptance of the updated Privacy Policy.</p>
           </div>
         </div>
       </section>
--- src/app/terms-of-use/page.js
+++ src/app/terms-of-use/page.js
@@ -8,37 +8,43 @@ import styles from "./page.module.css"
 const page = () => {
   return (
     <Fragment>
-      <PageHeader title="Terms Of Use" text="Terms Of Use" />
+      <PageHeader title="Terms & Conditions" text="Terms & Conditions" />
       <section className={styles.privacy_policy}>
         <div className="container">
           <div className={styles.content_meta}>
-            <h4>Lorem Ipsum</h4>
+            <h4>Terms and Conditions</h4>
             <p>
-              The Securitynet.ai ecosystem is powered by the SecNet Token (SNT),
-              an ERC-20 compatible token built on the Ethereum blockchain. The
-              SecNet Token plays a pivotal role in facilitating access to our
-              AI-powered security services and governing the platform&apos;s
-              operations. Here&apos;s an overview of the tokenomics structure:
+            Welcome to Quadruple Solutions. These Terms and Conditions ("Terms") govern your access to and use of our website, products, and services. By accessing or using our website or services, you agree to comply with these terms and conditions. If you do not agree with these terms, please do not access or use our website or services.
             </p>
-            <h4>Lorem Ipsum</h4>
+            <h4>1. Acceptance of Terms</h4>
             <ul className={styles.list}>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
+              <li>1.1 By accessing or using our website or services, you agree to be bound by these Terms and all applicable laws and regulations. If you do not agree with any of these Terms, you are prohibited from using our website and services.</li>
+              <li>1.2 We may modify these Terms at any time without prior notice. Your continued use of our website and services after any modifications indicates your acceptance of the updated Terms.</li>
             </ul>
-            <h4>Lorem Ipsum</h4>
-            <p>
-              The Securitynet.ai ecosystem is powered by the SecNet Token (SNT),
-              an ERC-20 compatible token built on the Ethereum blockchain. The
-              SecNet Token plays a pivotal role in facilitating access to our
-              AI-powered security services and governing the platform&apos;s
-              operations. Here&apos;s an overview of the tokenomics structure:
-            </p>
-            <h4>Lorem Ipsum</h4>
+            <h4>2. Use of Website</h4>
             <ul className={styles.list}>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
-              <li>Initial Token Allocation: 1.5 billion SNT to kens.</li>
+              <li>2.1 You may use our website for lawful purposes only. You agree not to use our website in any way that violates applicable laws or regulations.</li>
+              <li>2.2 You may not use our website to transmit, distribute, or store any material that is unlawful, harmful, defamatory, obscene, or otherwise objectionable.</li>
+              <li>2.3 You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.</li>
+            </ul>
+            <h4>3. Intellectual Property</h4>
+            <ul className={styles.list}>
+              <li>3.1 All content on our website, including text, graphics, logos, images, and software, is the property of Quadruple Solutions or its licensors and is protected by copyright and other intellectual property laws.</li>
+              <li>3.2 You may not reproduce, distribute, modify, or create derivative works of any content from our website without our prior written consent.</li>
+            </ul>
+            <h4>4. Disclaimer of Warranties</h4>
+            <ul className={styles.list}>
+              <li>4.1 Our website and services are provided on an "as is" and "as available" basis, without any warranties of any kind, express or implied.</li>
+              <li>4.2 We do not warrant that our website or services will be uninterrupted, error-free, or free of viruses or other harmful components.</li>
+            </ul>
+            <h4>5. Limitation of Liability</h4>
+            <ul className={styles.list}>
+              <li>5.1 To the fullest extent permitted by law, Quadruple Solutions shall not be liable for any indirect, incidental, special, consequential, or punitive damages arising out of or in connection with your use of our website or services.</li>
+              <li>5.2 In no event shall our total liability to you exceed the amount paid by you, if any, for accessing or using our website or services.</li>
+            </ul>
+            <h4>6. Changes to Terms</h4>
+            <ul className={styles.list}>
+              <li>6.1 We reserve the right to update or modify these Terms at any time. We will notify you of any changes by posting the new Terms on this page. Your continued use of our website or services after any such changes constitutes acceptance of the updated Terms.</li>
             </ul>
           </div>
         </div>
